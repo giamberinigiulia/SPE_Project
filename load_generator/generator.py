82885858c921474eda80df3d702a4c379175eb05
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-10-17 17:13:44 +0200

    Calculated response time

--- load_generator/generator.py
+++ load_generator/generator.py
@@ -6,6 +6,8 @@ import requests
 import threading
 import time
 from numpy import random
+from csv import writer
+import os
 
 
 class LoadGenerator:
@@ -18,22 +20,37 @@ class LoadGenerator:
         self.enter_rate = enter_rate
         self.max_time = max_time
         self.target_url = target_url
+
+        self.csv_file_name = 'response_time.csv'
+
         random.seed(10)
+
+        if os.path.exists(self.csv_file_name):
+            os.remove(self.csv_file_name)
 
     def __send_request(self) -> None:
         passed_time = 0
+        response_time = []
         while passed_time < self.max_time:
             start_time = time.time()
             try:
                 waiting_time = random.exponential(1/self.enter_rate)
-                print(waiting_time)
                 time.sleep(waiting_time)
                 response = requests.get(self.target_url)
+                if response.status_code == 200:     # ignore responses with an error
+                    response_time.append(response.elapsed.total_seconds())
                 print(f"Response: {response.status_code}, Time: {response.elapsed.total_seconds()}")
             except requests.exceptions.RequestException as e:
                 print(f"Error: {e}")
             end_time = time.time()
             passed_time += (end_time - start_time)
+        self.__write_csv(response_time)
+
+    def __write_csv(self, response_time: [float]) -> None:
+        with open(self.csv_file_name, 'a') as file:
+            wr = writer(file)
+            wr.writerow(response_time)
+            file.close()
 
     def generate_load(self) -> None:
         threads = []
--- load_generator/main.py
+++ load_generator/main.py
@@ -1,5 +1,5 @@
 from generator import LoadGenerator
 
-lg = LoadGenerator(10, 0.2, 20, "http://localhost")
+lg = LoadGenerator(5, 0.2, 5, "https://example.com/")
 lg.generate_load()
 print("End")
