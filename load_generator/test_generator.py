292b359fc17878d7cdd59c877eb080b6d99eb385
NicoPolazzi <nicolo.polazzi@gmail.com>
2024-10-18 17:08:43 +0200

    implemented generate_mean_response_times

--- load_generator/test_generator.py
+++ load_generator/test_generator.py
@@ -5,7 +5,7 @@ import csv
 from generator import LoadGenerator
 
 
-def plot_response_time_test(enter_rate: float, exit_rate: float) -> None:
+def plot_response_times_test(enter_rate: float, exit_rate: float) -> None:
     '''Test clients' requests independence
 
     '''
@@ -30,10 +30,14 @@ def generate_mean_response_times(enter_rate: float, max_time: int, number_rounds
     '''
     mean_response_times = []
 
-    for n in range(number_rounds):
-        lg = LoadGenerator(clients_number=n, enter_rate=enter_rate, max_time=max_time, target_url="https://example.com/")
-        
-    return
+    # We want to skip the case with 0 client because it makes no sense, maybe we can capture this corner case
+    for n in range(1, number_rounds):
+        lg = LoadGenerator(clients_number=n, enter_rate=enter_rate,
+                           max_time=max_time, target_url="https://example.com/")
+        mean_response_times.append(compute_mean_response_time(lg))
+
+    return mean_response_times
+
 
 def compute_mean_response_time(lg: LoadGenerator) -> float:
     # extract response times from the csv and compute the mean
@@ -41,10 +45,11 @@ def compute_mean_response_time(lg: LoadGenerator) -> float:
     response_time, number_of_responses = fetch_times_from_csv(lg.csv_file_name)
     return response_time/number_of_responses
 
+
 def fetch_times_from_csv(csv_filename: str) -> tuple[float, int]:
     response_time = 0.0
     number_of_responses = 0
-    
+
     with open(csv_filename, 'r', newline='') as response_time_csv:
         response_time_reader = csv.reader(response_time_csv, delimiter=',')
 
@@ -52,10 +57,8 @@ def fetch_times_from_csv(csv_filename: str) -> tuple[float, int]:
             for value in row:
                 response_time += float(value)
                 number_of_responses += 1
-    
+
     return response_time, number_of_responses
 
 
-lg = LoadGenerator(clients_number=5, enter_rate=0.2, max_time=5, target_url="https://example.com/")
-print(compute_mean_response_time(lg))
-#plot_response_time_test(0.2, 2)
+# print(generate_mean_response_times(0.2, 5, 5))
