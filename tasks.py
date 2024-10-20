5c38537ce110f855bdc9ff2ab799d1146b1d7d08
Emanuele Brizzi <brizziemanuele@gmail.com>
2024-10-20 07:32:35 +0000

    Canged the CPU bound task. Used Fibonacci Serie

--- tasks.py
+++ tasks.py
@@ -1,10 +1,11 @@
 import time
+"""
 import math
 
 class CPUBoundTask:
     @staticmethod
     def run(duration):
-        """Simulate a CPU-bound task."""
+        # Simulate a CPU-bound task.
         x = 12345.6789
         y = 98765.4321
         z = 0
@@ -12,4 +13,20 @@ class CPUBoundTask:
         while time.time() < end_time:
             z += math.sqrt(x) * math.sqrt(y)
             print(z)
+"""
 
+class CPUBoundTask:
+    @staticmethod
+    def run(duration):
+        # Simulate a CPU-bound task. Fibonacci serie
+        n = 10
+        end_time = time.time() + duration
+        while time.time() < end_time:
+            fibonacci(n)
+            print(n)
+
+def fibonacci(n):
+        if n <= 1:
+            return n
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
