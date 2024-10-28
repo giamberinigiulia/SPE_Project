df648bc515557f22aaec51117bac6c33bfaacc23
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-10-28 15:22:01 +0100

    Deleted main.py file

--- load_generator/main.py
+++
@@ -1,6 +0,0 @@
-from generator import LoadGenerator
-
-if __name__ == '__main__':
-    lg = LoadGenerator(number_clients=3, enter_rate=0.2, max_time=5, target_url="https://example.com/")
-    lg.generate_load()
-    print("End")
