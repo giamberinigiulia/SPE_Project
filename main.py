cb29c9bc6420a4e2a55cb2d2d71081d5b2e26b97
Emanuele Brizzi <brizziemanuele@gmail.com>
2024-11-04 22:26:34 +0000

    Created ConfigJsonManager.py file for writing and reading a config.json file. Added the subcommands to luch the main.py by values mode or by config.json file. Added the creation of config.json file in the data directory with the values of arguments.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import time
 import datetime
+import ConfigJsonManager
 from multiprocessing import Process
 from load_generator.generator import LoadGenerator
 from server.FlaskServer import FlaskServer
@@ -10,6 +11,7 @@ from server.FlaskServer import FlaskServer
 # CSV_FOLDER_PATH = "./data/csv"
 # IMAGES_FOLDER_PATH = "./data/images"
 FOLDER_PATH = "./data"
+URL = "http://127.0.0.1:5000"
 
 
 def start_server(mu_value, file_path, images_path):
@@ -18,7 +20,7 @@ def start_server(mu_value, file_path, images_path):
     server.run()
 
 
-def start_load_generator(client_number, enter_rate, max_time, data_folder_csv, target_url: str = "http://127.0.0.1:5000"):
+def start_load_generator(client_number, enter_rate, max_time, data_folder_csv, target_url: str = URL):
     # Create a LoadGenerator instance and run it
     lg = LoadGenerator(number_clients=client_number, enter_rate=enter_rate,
                        max_time=max_time, csv_directory=data_folder_csv, target_url=target_url)
@@ -26,25 +28,36 @@ def start_load_generator(client_number, enter_rate, max_time, data_folder_csv, t
 
 
 if __name__ == '__main__':
-    # parse the parameters <mu> <lambda> <maxtime> <n_client>
-    # Example: python main.py 10 2.5 1000 50
     # Note: the server should be started before running this script, and it should be accessible at http://127.0.0.1:5000
-
-    # py main.py -s <m> -c <l> <t> <n>
-
-    # py main.py -mu <m> -lambda <l> -maxtime <t> -nclients <n>
 
     parser = argparse.ArgumentParser(description="Process some parameters.")
 
-    # Adding arguments to the parser
-    parser.add_argument('-m', type=float, required=True, help='Parameter mu (e.g., rate of arrival)')
-    parser.add_argument('-l', type=float, required=True, dest='l', help='Parameter lambda (e.g., service rate)')
-    parser.add_argument('-t', type=float, required=True, help='Maximum time to run the simulation')
-    parser.add_argument('-n', type=int, required=True, help='Number of clients to simulate')
+    # Define two subcommands, "Values Parser" to pass values ​​manually through main.py v -m <mu> -l <lambda> -t <maxtime> -n <nclients>
+    # and "Json Parser" to pass values ​​through json file through argument -c.
+    subparsers = parser.add_subparsers(dest="mode", required=True)
 
-    # Parse the arguments
+    # Configuration "Values Parser": main.py v -m <mu> -l <lambda> -t <maxtime> -n <nclients>
+    arguments_parser = subparsers.add_parser("v", help="Values Parser mode: main.py v -m <mu> -l <lambda> -t <maxtime> -n <nclients>")
+    arguments_parser.add_argument('-m', type=float, required=True, help='Parameter mu (e.g., rate of arrival)')
+    arguments_parser.add_argument('-l', type=float, required=True, help='Parameter lambda (e.g., service rate)')
+    arguments_parser.add_argument('-t', type=float, required=True, help='Maximum time to run the simulation')
+    arguments_parser.add_argument('-n', type=int, required=True, help='Number of clients to simulate')
+
+    # Configuration "Json Parser": main.py j -c <json_file_path>
+    json_parser = subparsers.add_parser("j", help="Json Parser mode: main.py j -c <json_file_path>")
+    json_parser.add_argument("-c", type=str, required=True, help="Path of confi.json file")
+
     args = parser.parse_args()
 
+    # Checking the selected mode
+    if args.mode == "v":
+        mu_rate = args.m
+        lambda_rate = args.l
+        number_clients = args.n
+        max_time = args.t 
+
+    elif args.mode == "j":
+        mu_rate, lambda_rate, number_clients, max_time = ConfigJsonManager.read_json(args.c)
 
     # Create data folder based on current date and time
     current_time = datetime.datetime.now()
@@ -60,30 +73,14 @@ if __name__ == '__main__':
     os.makedirs(data_folder)
     os.makedirs(data_folder_csv)
     os.makedirs(data_folder_images)
+    
+    ConfigJsonManager.generate_json(mu_rate, lambda_rate, number_clients, max_time, data_folder)
 
-    '''
-    # Reset the data folder
-    if os.path.exists(CSV_FOLDER_PATH):
-        shutil.rmtree(CSV_FOLDER_PATH)
-        os.makedirs(CSV_FOLDER_PATH)
-    if os.path.exists(IMAGES_FOLDER_PATH):
-        shutil.rmtree(IMAGES_FOLDER_PATH)
-        os.makedirs(IMAGES_FOLDER_PATH)
-    
-    # retrieve from command line n_client, lambda, maxtime, mu
-    print(sys.argv)
-    mu = sys.argv[1]
-    enter_rate = float(sys.argv[2])
-    max_time = int(sys.argv[3])
-    client_number = int(sys.argv[4])
-    help_message = f"Usage: python main.py <mu> <lambda> <maxtime> <n_client>"
-    
-    '''
-    server = Process(target=start_server, args=[args.m, data_folder_csv, data_folder_images])
+    server = Process(target=start_server, args=[mu_rate, data_folder_csv, data_folder_images])
     server.start()
     time.sleep(2)
 
-    client = Process(target=start_load_generator, args=[args.n, args.l, args.t, data_folder_csv])
+    client = Process(target=start_load_generator, args=[number_clients, lambda_rate, max_time, data_folder_csv])
     client.start()
 
     client.join()
