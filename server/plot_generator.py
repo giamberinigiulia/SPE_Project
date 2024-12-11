1ada6a4d831d6950e03908828544db6f86556cb5
giuli <giuliagiamberini@gmail.com>
2024-12-11 10:44:34 +0100

    Modification of server threading

--- server/plot_generator.py
+++ server/plot_generator.py
@@ -9,22 +9,23 @@ class PlotGenerator:
         self.file_path = image_path
 
     def generate_exponential_plot(self, mu, mean_mu, values, n):
-        # int(10/mu) + np.log10(mu) in order to have enogh space to represent all data
-        # int(10/mu) guarantee that lower values have a correct representation when np.log10(mu) will be 0
-        # otherwise if the mu value is higher, np.log10(mu) will be reasonable and int(10/mu) will be zero
-
+        # Calculate time values for the plot
         time_values = np.linspace(0, int(10/mu) + np.log10(mu), 1000)
+        # Calculate the probability density function (PDF) for the exponential distribution
         pdf = mu * np.exp(-mu * time_values)
 
+        # Create the plot
         plt.figure(figsize=(8, 6))
         plt.hist(values, bins=np.linspace(0, int(10/mu) + np.log10(mu), 30), density=True)
         plt.plot(time_values, pdf, label=f'PDF (mu = {mu:.2f})')
 
+        # Add labels, title, legend, and grid to the plot
         plt.xlabel('Time (seconds)')
         plt.ylabel('Probability Density')
         plt.title(f'Exponential Distribution with Rate {mu} vs {mean_mu:.2f} with {n} trials')
         plt.legend()
         plt.grid(True)
-        # check this becoause the path isn't consistent and loclahost:5000/plot raise a 500 server error
+
+        # Save the plot to the specified file path and close the plot
         plt.savefig(self.file_path)
         plt.close()
