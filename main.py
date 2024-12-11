7554e6950b544ec476def3a83b9f7ac7f4244543
giuli <giuliagiamberini@gmail.com>
2024-12-11 11:56:40 +0100

    Comments on unused functions

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ FOLDER_PATH = "./data"
 URL = "http://127.0.0.1:5000"
 
 
-def start_server(mu_value, file_path, images_path, server_count):
+def start_server(mu_value, server_count):
     # Create a Server instance and run it
-    server = FlaskServer(mu_value, file_path, images_path, server_count)
+    server = FlaskServer(mu_value, server_count)
     server.run()
 
 
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     service_rate, arrival_rate, user_range, user_request_time, server_count = arg.parse_arguments(parser)
 
     # Create data folder based on current date and time
-    current_time = datetime.datetime.now()
+    '''current_time = datetime.datetime.now()
     dt = current_time.strftime('%Y%m%d') + '_' + current_time.strftime('%H%M%S')
 
     data_folder = FOLDER_PATH + "/" + dt
@@ -36,11 +36,12 @@ if __name__ == '__main__':
         shutil.rmtree(data_folder)
     os.makedirs(data_folder)
     os.makedirs(data_folder_csv)
-    os.makedirs(data_folder_images)
+    os.makedirs(data_folder_images)'''
 
     # manager.generate_json(service_rate, arrival_rate, user_range, user_request_time, data_folder)
 
-    server = Process(target=start_server, args=[service_rate, data_folder_csv, data_folder_images, server_count])
+    # server = Process(target=start_server, args=[service_rate, data_folder_csv, data_folder_images, server_count])
+    server = Process(target=start_server, args=[service_rate, server_count])
     server.start()
     time.sleep(2)
 
--- server/flask_server.py
+++ server/flask_server.py
@@ -5,9 +5,9 @@ import os                                    # For file operations (deleting fil
 import random                                # For setting random seed
 import numpy as np
 
-from server.delay_analyzer import DelayAnalyzer
+# from server.delay_analyzer import DelayAnalyzer
 from server.cpubound_task import CPUBoundTask
-from server.plot_generator import PlotGenerator
+# from server.plot_generator import PlotGenerator
   
 
 class FlaskServer:
@@ -22,8 +22,8 @@ class FlaskServer:
     first_call = True
 
 
-    def __init__(self, mu_value: float = 10.0, file_path: str = "./data/csv", image_path: str = "./data/images", server_count: int = 1):
-        
+    # def __init__(self, mu_value: float = 10.0, file_path: str = "./data/csv", image_path: str = "./data/images", server_count: int = 1):
+    def __init__(self, mu_value: float = 10.0, server_count: int = 1):
         # Initialize the Flask application within the class
         self.app = Flask(__name__)
 
@@ -33,14 +33,14 @@ class FlaskServer:
         self.mu_value = float(mu_value)
 
         # Set paths based on mu_value
-        extensionFileName = f"_{str(self.mu_value).split('.')[0]}_{str(self.mu_value).split('.')[1]}"
-        self.csv_path = f"{file_path}/request_delays{extensionFileName}.csv"
-        self.images_path = f"{image_path}/plot{extensionFileName}.png"
+        # extensionFileName = f"_{str(self.mu_value).split('.')[0]}_{str(self.mu_value).split('.')[1]}"
+        # self.csv_path = f"{file_path}/request_delays{extensionFileName}.csv"
+        # self.images_path = f"{image_path}/plot{extensionFileName}.png"
 
         # Store configurations in the Flask app
         self.app.config['MU'] = self.mu_value
-        self.app.config['csv_path'] = self.csv_path
-        self.app.config['images_path'] = self.images_path
+        # self.app.config['csv_path'] = self.csv_path
+        # self.app.config['images_path'] = self.images_path
 
         # Initialize routes
         self.__setup_routes()
@@ -50,8 +50,8 @@ class FlaskServer:
         mu = self.app.config.get('MU')
 
         # inizialize the delay_analyzer and the plot_generator with file names
-        delay_analyzer = DelayAnalyzer(self.app.config['csv_path'])
-        plot_generator = PlotGenerator(self.app.config['images_path'])
+        # delay_analyzer = DelayAnalyzer(self.app.config['csv_path'])
+        # plot_generator = PlotGenerator(self.app.config['images_path'])
 
         @self.app.route('/', methods=['GET'])
         def process_task():
@@ -69,7 +69,7 @@ class FlaskServer:
             CPUBoundTask.run(delay)
             # end_time = time.time()
             # delay_analyzer.log_delay_to_csv(mu, end_time-start_time)
-            #delay_analyzer.log_delay_to_csv(mu, delay)
+            # delay_analyzer.log_delay_to_csv(mu, delay)
             self.server_active_time += time.time() - start_processing
             return jsonify({"message": "Task completed", "duration": delay})
 
@@ -94,14 +94,4 @@ class FlaskServer:
         # Start the Flask application without multithreading
         print(f"Starting Flask app with mu = {self.mu_value}")
         self.app.run(threaded=False, processes = self.k_server)
-
-
-if __name__ == '__main__':
-    # Example usage: instantiate the Server with a specific mu_value
-    if len(sys.argv) != 2:
-        print("Usage: python server.py <mu_value>")
-        sys.exit(1)
-
-    mu_value = sys.argv[1]
-    server = FlaskServer(mu_value)  # Instantiate with the provided mu_value
-    server.run()                    # Run the Flask app
+        
