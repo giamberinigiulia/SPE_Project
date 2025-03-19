359a30bbf48f066eea9e12147cee07b0451ff3c2
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-03-19 21:33:13 +0100

    Working multi-servants

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import time
 from multiprocessing import Process
 
+import requests
+
 from spe.generator import simulation
 import spe.argument_parser as arg
-from spe.server.flask_server import FlaskServer
 
-# Maybe is possible to move this class in flask server module
-def start_server(system_config: arg.Config) -> None:
-    # Create a Server instance and run it
-    server = FlaskServer(system_config.service_rate, system_config.number_of_servers)
-    server.run()
-
+TARGET_URL = "http://127.0.0.1:5000"
 
 if __name__ == '__main__':
     parser = arg.create_parser()
     system_config = arg.parse_arguments(parser)
 
-    server = Process(target=start_server, args=[system_config])
-    server.start()
-    time.sleep(2)
-
+    response = requests.get(TARGET_URL + "/mu/" + str(system_config.service_rate))
+    print(response.text)
     simulation.start_load_simulation(system_config)
-    server.join()
+
+"""
+gunicorn -w 4 -b 127.0.0.1:5000 --access-logfile access.log --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s %(p)s' --error-logfile error.log spe.server.flask_server:app 
+gunicorn -w 4 -b 127.0.0.1:5000 --access-logfile access.log --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s %(p)s %(L)s' --error-logfile error.log spe.server.flask_server:app
+"""
